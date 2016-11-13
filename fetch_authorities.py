68d03d1e9479db9886f556cbe48255e3287f3f44
Jane Sandberg <sandbergja@users.noreply.github.com>
2016-11-12 22:25:36 -0800

    various refinements

--- fetch_authorities.py
+++ fetch_authorities.py
@@ -1,7 +1,8 @@
 from opensearch import Client
 from pymarc import MARCWriter, marcxml
 from string import punctuation
-import re, sys, urllib
+from xml.sax._exceptions import SAXParseException
+import os, re, sys, time, urllib
 
 if len(sys.argv) < 2:
     print("Please include the filename")
@@ -14,15 +15,23 @@ try:
         for term in authorized_terms:
             print term
             response = search_client.search(term.lstrip().rstrip('.,;'))
+            i = 0
             for result in response:
-                auth_id = re.search('.*\/authorities\/.*\/(.*)', result.link).group(1)
-                auth_file_url = 'http://lccn.loc.gov/' + auth_id + '/marcxml'
-                urllib.urlretrieve (auth_file_url, "tmp.marcxml")
-                records = marcxml.parse_xml_to_array('tmp.marcxml')
-                writer = MARCWriter(file('authorities_to_load.mrc', 'ab'))
-                for r in records:
-                    writer.write(r)
-                    writer.close() 
+                if i < 6:
+                    i = i + 1
+                    auth_id = re.search('.*\/authorities\/.*\/(.*)', result.link).group(1)
+                    auth_file_url = 'http://lccn.loc.gov/' + auth_id + '/marcxml'
+                    time.sleep(20)
+                    urllib.urlretrieve (auth_file_url, 'tmp.marcxml')
+                    try:
+                        records = marcxml.parse_xml_to_array('tmp.marcxml')
+                        writer = MARCWriter(file('authorities_to_load.mrc', 'ab'))
+                        for r in records:
+                            writer.write(r)
+                            writer.close()
+                        os.remove('tmp.marcxml')
+                    except SAXParseException:
+                        print('Could not parse MARCXML file')
 except IOError:
     print("File '%s' doesn't exist", fname)
     exit(1)
