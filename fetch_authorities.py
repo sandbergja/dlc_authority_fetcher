e3b1d4b1c22f9f57f88d60854c39d9ab6c875a2a
Jane Sandberg <sandbej@linnbenton.edu>
2018-03-10 07:31:51 -0800

    Allows user to limit by vocabulary

--- fetch_authorities.py
+++ fetch_authorities.py
@@ -7,14 +7,27 @@ import argparse, os, re, sys, time, urllib
 parser = argparse.ArgumentParser(description='Fetch MARC authority records from the Library of Congress')
 parser.add_argument('filename', help='a file with headings on separate lines')
 parser.add_argument('--matches', type=int, default=5, help='maximum number of matching records to download for each heading')
+vocabularies = parser.add_mutually_exclusive_group()
+vocabularies.add_argument('--lcnaf-only', help='only download name authority records', action='store_true')
+vocabularies.add_argument('--lcsh-only', help='only download subject authority records', action='store_true')
+vocabularies.add_argument('--lcgft-only', help='only download genre/form authority records', action='store_true')
 args = parser.parse_args()
 
 try:
     search_client = Client('http://id.loc.gov/opensearch/')
     with open(args.filename) as authorized_terms:
+        if args.lcnaf_only:
+            heading_suffix = ' scheme:http://id.loc.gov/authorities/names'
+        elif args.lcsh_only:
+            heading_suffix = ' scheme:http://id.loc.gov/authorities/subjects'
+        elif args.lcgft_only:
+            heading_suffix = ' scheme:http://id.loc.gov/authorities/genreForms'
+        else:
+            heading_suffix = ''
+
         for term in authorized_terms:
             print(term)
-            response = search_client.search(term.lstrip().rstrip('.,;'))
+            response = search_client.search(term.lstrip().rstrip('.,;') + heading_suffix)
             i = 0
             for result in response:
                 if i < args.matches:
