57dc7587c41d9f39dddffdb75e354ce085aa9a12
Jane Sandberg <sandbej@linnbenton.edu>
2020-01-09 22:14:52 -0800

    Only search authorities, not works and other records

--- fetch_authorities.py
+++ fetch_authorities.py
@@ -13,6 +13,8 @@ vocabularies = parser.add_mutually_exclusive_group()
 vocabularies.add_argument('--lcnaf-only', help='only download name authority records', action='store_true')
 vocabularies.add_argument('--lcsh-only', help='only download subject authority records', action='store_true')
 vocabularies.add_argument('--lcgft-only', help='only download genre/form authority records', action='store_true')
+vocabularies.add_argument('--lcdgt-only', help='only download demographic group terms authority records', action='store_true')
+vocabularies.add_argument('--childrens-only', help='only download childrens subject authority records', action='store_true')
 print_opts = parser.add_argument_group(title='Output options')
 print_opts.add_argument('--print-matched-headings', '-m', help='Print headings that are matched and downloaded to STDOUT', action='store_true')
 print_opts.add_argument('--print-unmatched-headings', '-u', help='Print headings that have no LC matches to STDOUT', action='store_true')
@@ -21,46 +23,58 @@ args = parser.parse_args()
 try:
     search_client = Client('http://id.loc.gov/opensearch/')
     with open(args.filename) as authorized_terms:
+        authorities = {
+                'lcnaf': ' scheme:http://id.loc.gov/authorities/names',
+                'lcsh': ' scheme:http://id.loc.gov/authorities/subjects',
+                'lcgft': ' scheme:http://id.loc.gov/authorities/genreForms',
+                'lcdgt': ' scheme:http://id.loc.gov/authorities/demographicTerms',
+                'childrens': ' scheme:http://id.loc.gov/authorities/childrensSubjects',
+        }
         if args.lcnaf_only:
-            heading_suffix = ' scheme:http://id.loc.gov/authorities/names'
+            suffixes = [authorities['lcnaf']]
         elif args.lcsh_only:
-            heading_suffix = ' scheme:http://id.loc.gov/authorities/subjects'
+            suffixes = [authorities['lcsh']]
         elif args.lcgft_only:
-            heading_suffix = ' scheme:http://id.loc.gov/authorities/genreForms'
+            suffixes = [authorities['lcgft']]
+        elif args.lcdgt_only:
+            suffixes = [authorities['lcdgt']]
+        elif args.childrens_only:
+            suffixes = [authorities['childrens']]
         else:
-            heading_suffix = ''
+            suffixes = authorities.values()
 
         for term in authorized_terms:
             if args.exact:
                 term_for_search = 'aLabel:"' + term.lstrip().rstrip() + '"'
             else:
                 term_for_search = term.lstrip().rstrip('.,;')
-            response = search_client.search(term_for_search + heading_suffix)
-            if response.totalResults:
-                if args.print_matched_headings:
+            for heading_suffix in suffixes:
+                response = search_client.search(term_for_search + heading_suffix)
+                if (response.totalResults):
+                    i = 0
+                    for result in response:
+                        if i < args.matches:
+                            i = i + 1
+                            if 'authorities' in result.link:
+                                auth_id = re.search('.*\/authorities\/.*\/(.*)', result.link).group(1)
+                                auth_file_url = 'http://lccn.loc.gov/' + auth_id + '/marcxml'
+                                time.sleep(10)
+                                urllib.urlretrieve (auth_file_url, 'tmp.marcxml')
+                                try:
+                                    if args.print_matched_headings:
+                                        print(term)
+                                    records = marcxml.parse_xml_to_array('tmp.marcxml')
+                                    writer = MARCWriter(file(args.output, 'ab'))
+                                    for r in records:
+                                        writer.write(r)
+                                        writer.close()
+                                    os.remove('tmp.marcxml')
+                                except SAXParseException:
+                                    print('Could not parse MARCXML file')
+                        else:
+                           break
+                elif args.print_unmatched_headings:
                     print(term)
-                i = 0
-                for result in response:
-                    if i < args.matches:
-                        i = i + 1
-                        if 'authorities' in result.link:
-                            auth_id = re.search('.*\/authorities\/.*\/(.*)', result.link).group(1)
-                            auth_file_url = 'http://lccn.loc.gov/' + auth_id + '/marcxml'
-                            time.sleep(10)
-                            urllib.urlretrieve (auth_file_url, 'tmp.marcxml')
-                            try:
-                                records = marcxml.parse_xml_to_array('tmp.marcxml')
-                                writer = MARCWriter(file(args.output, 'ab'))
-                                for r in records:
-                                    writer.write(r)
-                                    writer.close()
-                                os.remove('tmp.marcxml')
-                            except SAXParseException:
-                                print('Could not parse MARCXML file')
-                    else:
-                        break
-            elif args.print_unmatched_headings:
-                print(term)
 except IOError:
     print("File '%s' doesn't exist", args.filename)
     exit(1)
